ba1cb8641e483ace55013b2b4b56f55a3027b234
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-25 00:09:15 +0300

    Created the basic layout.

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,26 @@
+//image paths
+import 'package:flutter/material.dart';
+
+const String kBGPath = "assets/images/vincent_bg.jpg";
+
+//texts
+const String kVincentVanGoghText = "Vincent\nvan Gogh";
+const String kDateText = "30 March 1853-29 July 1890";
+const String kExplanationText =
+    "Vincent Willem van Gogh was a Dutch post-impressionist painter who posthumously became one of the most famous and influential figures in the history of Western art.";
+const String kHighlightText = "Highlight Paintings";
+
+//colors
+const Color kPrimaryWhiteColor = const Color(0xffF8F3F7);
+
+//text styles
+const TextStyle kTitleStyle = const TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w700,
+  color: kPrimaryWhiteColor,
+);
+const TextStyle kNormalTextStyle = const TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.w500,
+  color: kPrimaryWhiteColor,
+);
---
+++ lib/views/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:parallax_effect/utilities/constants.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: Get.height,
+        width: Get.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(kBGPath),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: buildBody(),
+        ),
+      ),
+    );
+  }
+
+  Padding buildBody() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Expanded(
+            flex: 3,
+            child: const Text(
+              kVincentVanGoghText,
+              style: kTitleStyle,
+            ),
+          ),
+          const Expanded(
+            flex: 1,
+            child: const Text(
+              kDateText,
+              style: kNormalTextStyle,
+            ),
+          ),
+          const Expanded(
+            flex: 3,
+            child: const Text(
+              kExplanationText,
+              style: kNormalTextStyle,
+              textAlign: TextAlign.justify,
+            ),
+          ),
+          Expanded(
+            flex: 12,
+            child: const SizedBox(),
+          ),
+          const Expanded(
+            flex: 1,
+            child: const Align(
+              alignment: Alignment.center,
+              child: const Text(
+                kHighlightText,
+                style: kNormalTextStyle,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
